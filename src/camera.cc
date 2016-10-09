1ae2c4ed2efa66f6b718fc2b66850d46c925ff84
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2016-10-09 13:51:57 -0500

    Implemented lookat matrix

--- src/camera.cc
+++ src/camera.cc
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <sstream>
 #include "camera.h"
 
 namespace {
@@ -7,8 +9,22 @@ namespace {
 	float zoom_speed = 0.1f;
 };
 
+std::string matrixString(const glm::mat4& mat);
+
 // FIXME: Calculate the view matrix
 glm::mat4 Camera::get_view_matrix() const
 {
-	return glm::mat4();
+    glm::vec3 Z = glm::normalize(eye_ - look_);
+    glm::vec3 X = glm::cross(up_, Z);
+    glm::vec3 Y = glm::normalize(glm::cross(Z, X));
+    X = glm::normalize(X);
+
+    // Yes the first three could have been in a loop but SHUSH!
+    glm::mat4 eyeMat(1.0f);
+    eyeMat[0] = glm::vec4(X.x, Y.x, Z.x, 0);
+    eyeMat[1] = glm::vec4(X.y, Y.y, Z.y, 0);
+    eyeMat[2] = glm::vec4(X.z, Y.z, Z.z, 0);
+    eyeMat[3] = glm::vec4(glm::dot(-X, eye_), glm::dot(-Y, eye_), glm::dot(-Z, eye_), 1);
+    
+    return eyeMat;
 }

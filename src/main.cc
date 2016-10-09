1d6680ed8c0331ae442abef77f4763f748d4ad5e
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2016-10-09 16:22:10 -0500

    Added partial coloring (need to pass in world, not camera, coordinate normals)

--- src/main.cc
+++ src/main.cc
@@ -56,7 +56,7 @@ in vec4 light_direction;
 out vec4 fragment_color;
 void main()
 {
-	vec4 color = vec4(1.0, 0.0, 0.0, 1.0);
+	vec4 color = vec4(1.0*normal.x, 1.0*normal.y, 1.0*normal.z, 1.0);
 	float dot_nl = dot(normalize(light_direction), normalize(normal));
 	dot_nl = clamp(dot_nl, 0.0, 1.0);
 	fragment_color = clamp(dot_nl * color, 0.0, 1.0);

bbe220746351fc9e2283037cfd7bc34c3c6072f5
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2016-10-12 16:51:37 -0500

    Created preliminary input

--- src/input.cpp
+++ src/input.cpp
@@ -3,3 +3,104 @@
 //
 
 #include "input.hpp"
+
+MouseState::MouseState(GLFWwindow*) {
+
+}
+
+bool MouseState::pressed(unsigned button) {
+    currMutex.lock();
+    bool result = button >= N_MOUSE_BUTTONS ? false : curr[button];
+    currMutex.unlock();
+    return result;
+}
+
+bool MouseState::released(unsigned button) {
+    currMutex.lock();
+    bool result = button >= N_MOUSE_BUTTONS ? false : !curr[button];
+    currMutex.unlock();
+    return result;
+}
+
+bool MouseState::justPressed(unsigned button) {
+    currMutex.lock();
+    bool result =  button >= N_MOUSE_BUTTONS ? false : curr[button] && !prev[button];
+    currMutex.unlock();
+    return result;
+}
+
+bool MouseState::justReleased(unsigned button) {
+    currMutex.lock();
+    bool result = button >= N_MOUSE_BUTTONS ? false : !curr[button] && prev[button];
+    currMutex.unlock();
+    return result;
+}
+
+glm::vec2 MouseState::getPos() {
+    posMutex.lock();
+    glm::vec2 res = pos;
+    posMutex.unlock();
+    return res;
+}
+
+glm::vec2 MouseState::delPos() {
+    posMutex.lock();
+    glm::vec2 res = pos - prevPos;
+    posMutex.unlock();
+    return res;
+}
+
+MouseState::~MouseState() {
+
+}
+
+void MouseState::update() {
+    currMutex.lock();
+    pos = prevPos;
+    for(unsigned i = 0; i < N_MOUSE_BUTTONS; i++)
+        prev[i] = curr[i];
+    currMutex.unlock();
+}
+
+
+KeyState::KeyState(GLFWwindow* window) {
+
+}
+
+bool KeyState::pressed(unsigned button) {
+    currMutex.lock();
+    bool result = button >= N_KEY_BUTTONS ? false : curr[button];
+    currMutex.unlock();
+    return result;
+}
+
+bool KeyState::released(unsigned button) {
+    currMutex.lock();
+    bool result = button >= N_KEY_BUTTONS ? false : !curr[button];
+    currMutex.unlock();
+    return result;
+}
+
+bool KeyState::justPressed(unsigned button) {
+    currMutex.lock();
+    bool result =  button >= N_KEY_BUTTONS ? false : curr[button] && !prev[button];
+    currMutex.unlock();
+    return result;
+}
+
+bool KeyState::justReleased(unsigned button) {
+    currMutex.lock();
+    bool result = button >= N_KEY_BUTTONS ? false : !curr[button] && prev[button];
+    currMutex.unlock();
+    return result;
+}
+
+
+void KeyState::update() {
+    for(unsigned i = 0; i < N_KEY_BUTTONS; i++)
+        prev[i] = curr[i];
+}
+
+KeyState::~KeyState() {
+
+}
--- src/input.hpp
+++ src/input.hpp
@@ -2,39 +2,57 @@
 // Created by rdelfin on 10/11/16.
 //
 
+#include <mutex>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
 #ifndef GLSL_INPUT_HPP
 #define GLSL_INPUT_HPP
+
+#define N_MOUSE_BUTTONS 8
+#define N_KEY_BUTTONS (GLFW_KEY_LAST + 1)
 
 class MouseState {
 public:
     MouseState(GLFWwindow*);
-    bool pressed(int button);
-    bool released(int button);
-    bool justPressed(int button);
-    bool justReleased(int button);
+    bool pressed(unsigned button);
+    bool released(unsigned button);
+    bool justPressed(unsigned button);
+    bool justReleased(unsigned button);
 
-    glm::vec2 pos();
+    glm::vec2 getPos();
     glm::vec2 delPos();
+
+    void update();
 
     ~MouseState();
 private:
-    bool curr[8];   // GLFW defines up to 8 mouse buttons
-    bool prev[8];
-    glm::vec4 pos;
-    glm::vec4 prevPos;
+    bool curr[N_MOUSE_BUTTONS];   // GLFW defines up to 8 mouse buttons
+    bool prev[N_MOUSE_BUTTONS];
+    glm::vec2 pos;
+    glm::vec2 prevPos;
+
+    std::mutex currMutex;
+    std::mutex posMutex;
 };
 
 class KeyState {
 public:
     KeyState(GLFWwindow*);
-    bool pressed(int button);
-    bool released(int button);
-    bool justPressed(int button);
-    bool justReleased(int button);
+
+    bool pressed(unsigned button);
+    bool released(unsigned button);
+    bool justPressed(unsigned button);
+    bool justReleased(unsigned button);
+
+    void update();
+
     ~KeyState();
 private:
-    bool curr[GLFW_KEY_LAST + 1];
-    bool prev[GLFW_KEY_LAST + 1];
+    bool curr[N_KEY_BUTTONS];
+    bool prev[N_KEY_BUTTONS];
+
+    std::mutex currMutex;
 
 };
 
